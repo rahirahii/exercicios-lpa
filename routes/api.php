45d9016465864a0f8f851415cea53793580da684
rahirahii <rahiiany.viclarry@gmail.com>
2024-03-08 10:40:28 -0300

    exercicio 6

--- routes/api.php
+++ routes/api.php
@@ -36,3 +36,10 @@ Route::get('operacao2', function(Request $request){
     $resultado=$primeiroNumero-$segundoNumero-$terceiroNumero; 
     return $resultado; 
 }); 
+
+Route::get('operacao3', function(Request $request){ 
+    $primeiroNumero=$request->input('number1'); 
+    $segundoNumero=$request->input('number2'); 
+    $resultado=$primeiroNumero/$segundoNumero; 
+    return $resultado; 
+}); 
