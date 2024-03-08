af6900b74e514c6f0e7ad95fc288cb340811808a
rahirahii <rahiiany.viclarry@gmail.com>
2024-03-08 10:23:22 -0300

    exercicio 2

--- routes/api.php
+++ routes/api.php
@@ -4,8 +4,12 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('receber/nome', function(Request $request){ 
+   $nome=$request->input('name'); 
+return $nome; 
+}); 
 
-    $nome=$request->input('name'); 
-
-    return $nome; 
+Route::get('receber/nome/idade', function(Request $request){ 
+   $nome=$request->input('name'); 
+   $idade=$request->input('idade'); 
+return 'meu nome é '. $nome .' tenho'.$idade.' anos de idade'; 
 }); 
